0b6385b9048d3f1a24b7570a59dddb01118ee4fa
dylandinulos <819176@seq.org>
2026-05-04 00:02:27 +0000

    enemy.java changes

--- Enemy.java
+++ Enemy.java
@@ -1,5 +1,7 @@
  
 
+package main;
+
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -10,7 +12,7 @@ public class Enemy{
     int maxHealth;
     //int reward;   ---  for later
     
-
+    
     public Enemy(double x, double y, double speed, int health) {
         this.x = x;
         this.y = y;
@@ -18,6 +20,10 @@ public class Enemy{
         this.health = health;
         this.maxHealth = health;
         //this.reward = reward;
+    }
+
+    public Enemy(double x, double y) {
+        this(x, y, 1.5, 10);
     }
 
     public void update() {
