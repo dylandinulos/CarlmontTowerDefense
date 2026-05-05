ddb9f7fb3941420c6c94b2e481097625e143b5ea
isaacgrinker12-dev <isaac.grinker12@gmail.com>
2026-05-05 14:12:15 -0700

    Fix formatting issues in GameScreen.java

--- GameScreen.java
+++ GameScreen.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+a
 public class GameScreen extends JPanel{
 
     private Random random;
