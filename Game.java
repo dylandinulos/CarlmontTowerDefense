247c1a1367d71e479a3127473f26d786b4f8675a
isaacgrinker12-dev <isaac.grinker12@gmail.com>
2026-05-22 15:04:24 -0700

    Refactor Game class for improved readability and layout

--- Game.java
+++ Game.java
@@ -1,40 +1,56 @@
 package main;
 
-
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
 
-import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class Game extends JFrame 
+public class Game extends JFrame
 {
-
     private GameScreen gameScreen;
+
     private BufferedImage img;
+
     private StartScreen startScreen;
 
-    public enum STATE{
+    public enum STATE
+    {
         MENU,
         GAME,
         END
     };
-    
-    public static STATE State = STATE.MENU;
-    public Game() {
-        setSize(640,640);
-        
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    public static STATE State =
+        STATE.MENU;
+
+    public Game()
+    {
+        // Better size for MacBook Air
+        setSize(1440, 900);
+
+        setTitle(
+            "Carlmont Tower Defense"
+        );
+
+        setDefaultCloseOperation(
+            EXIT_ON_CLOSE
+        );
+
         setLocationRelativeTo(null);
-        gameScreen = new GameScreen();
+
+        setResizable(false);
+
+        gameScreen =
+            new GameScreen();
+
         add(gameScreen);
+
         setVisible(true);
     }
 
-
-
-    public static void main(String[] args) {
+    public static void main(
+        String[] args
+    )
+    {
         Game game = new Game();
     }
 }
