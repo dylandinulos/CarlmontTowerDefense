dc52683404d7400dd6d8858ff64490268a02c292
819494-dot <819494@seq.org>
2026-05-27 00:05:10 -0700

    Refactor rendering code for better readability

--- StartScreen.java
+++ StartScreen.java
@@ -14,118 +14,52 @@ public class StartScreen
 
     public void loadImage(String fileName)
     {
-        URL imageURL =
-            getClass().getResource(fileName);
+        URL imageURL = getClass().getResource(fileName);
 
         if(imageURL != null)
         {
-            logo =
-                new ImageIcon(imageURL)
-                .getImage();
+            logo = new ImageIcon(imageURL).getImage();
         }
     }
 
     public void render(Graphics g)
     {
-        // Background
-        Color background =
-            new Color(200, 200, 200);
+        Color background = new Color(200, 200, 200);
 
         g.setColor(background);
 
-        g.fillRect(
-            0,
-            0,
-            1440,
-            900
-        );
+        g.fillRect(0, 0, 1440, 900);
 
-        // Title
-        Font titleFont =
-            new Font(
-                "Arial",
-                Font.BOLD,
-                64
-            );
+        Font titleFont = new Font("Arial", Font.BOLD, 64);
 
         g.setFont(titleFont);
 
         g.setColor(Color.BLACK);
 
-        g.drawString(
-            "Carlmont Tower Defense",
-            320,
-            220
-        );
+        g.drawString("Carlmont Tower Defense", 320, 220);
 
-        // Main box
-        g.setColor(
-            new Color(
-                60,
-                60,
-                60
-            )
-        );
+        g.setColor(new Color(60, 60, 60));
 
-        g.fillRoundRect(
-            420,
-            420,
-            600,
-            160,
-            30,
-            30
-        );
+        g.fillRoundRect(420, 420, 600, 160, 30, 30);
 
-        // Border
         g.setColor(Color.WHITE);
 
-        g.drawRoundRect(
-            420,
-            420,
-            600,
-            160,
-            30,
-            30
-        );
+        g.drawRoundRect(420, 420, 600, 160, 30, 30);
 
-        // Instructions
-        Font infoFont =
-            new Font(
-                "Arial",
-                Font.PLAIN,
-                32
-            );
+        Font infoFont = new Font("Arial", Font.PLAIN, 32);
 
         g.setFont(infoFont);
 
-        g.drawString(
-            "Click Anywhere to Start",
-            520,
-            500
-        );
+        g.drawString("Click Anywhere to Start", 520, 500);
 
-        g.drawString(
-            "Defend the school from enemies!",
-            470,
-            550
-        );
+        g.drawString("Defend the school from enemies!", 470, 550);
 
-        // Bottom text
-        Font bottomFont =
-            new Font(
-                "Arial",
-                Font.PLAIN,
-                22
-            );
+        Font bottomFont = new Font("Arial", Font.PLAIN, 22);
 
         g.setFont(bottomFont);
 
         g.setColor(Color.BLACK);
 
-        g.drawString(
-            "Built with Java Swing",
-            590,
-            820
-        );
+        g.drawString("Built with Java Swing", 590, 820);
     }
 }
