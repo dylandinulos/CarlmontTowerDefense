5b3a4f3b7cbd510df6e07ac11f53d6c1d2d893fd
819494-dot <819494@seq.org>
2026-05-27 00:38:12 -0700

    Add image loading for different enemy types

--- Enemy.java
+++ Enemy.java
@@ -3,11 +3,17 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Image;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
 
 public class Enemy
 {
     double x, y;
     double speed;
+
+    private Image image;
 
     int health;
     int maxHealth;
@@ -18,7 +24,7 @@ public class Enemy
 
     int pathIndex = 0;
 
-    public Enemy(Point[] path, double speed, int health)
+    public Enemy(Point[] path, double speed, int health, String type)
     {
         this.path = path;
         this.speed = speed;
@@ -28,6 +34,23 @@ public class Enemy
 
         x = path[0].x;
         y = path[0].y;
+
+        if(type.equals("tank"))
+        {
+            loadImage("tank.png");
+        }
+        else if(type.equals("fast"))
+        {
+            loadImage("fast.png");
+        }
+        else if(type.equals("boss"))
+        {
+            loadImage("boss.png");
+        }
+        else
+        {
+            loadImage("normal.png");
+        }
     }
 
     public void update()
@@ -69,12 +92,36 @@ public class Enemy
     {
         int size = 28;
 
-        g.setColor(Color.RED);
+        if(image != null)
+        {
+            g.drawImage(image, (int)x - size / 2, (int)y - size / 2, size, size, null);
+        }
+        else
+        {
+            g.setColor(Color.RED);
 
-        g.fillOval((int)x - size / 2, (int)y - size / 2, size, size);
+            g.fillOval((int)x - size / 2, (int)y - size / 2, size, size);
 
-        g.setColor(Color.BLACK);
+            g.setColor(Color.BLACK);
 
-        g.drawOval((int)x - size / 2, (int)y - size / 2, size, size);
+            g.drawOval((int)x - size / 2, (int)y - size / 2, size, size);
+        }
+    }
+
+    private void loadImage(String fileName)
+    {
+        try
+        {
+            URL imageURL = getClass().getResource(fileName);
+
+            if(imageURL != null)
+            {
+                image = new ImageIcon(imageURL).getImage();
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
